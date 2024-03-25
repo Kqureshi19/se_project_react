875281392454645cdb82dbf849e9f58b22282d1f
Kqureshi19 <Kqureshi3@yahoo.com>
2024-03-25 16:35:37 -0400

    update App.js the Footer file path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import logo from "../../logo.svg";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main.js";
-import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer.js";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import { useState } from "react";
 import ItemModal from "../ItemModal/ItemModal";
